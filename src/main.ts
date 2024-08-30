83117b3310687fee75e54485c961d834ebb3b645
walttinho <walter.netto@live.com>
2024-08-29 22:38:54 -0300

    docs: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Water and Gas Meter Reading API')
+    .setDescription(
+      'API documentation for managing water and gas meter readings.',
+    )
+    .setVersion('1.0')
+    .addTag('Measure')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
